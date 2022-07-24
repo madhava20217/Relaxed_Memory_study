291a8a24445cc5802046eef9752dadd3e50e9821
Chameleon Cloud User <cc@ch-cascade.novalocal>
2022-07-24 05:01:53 +0000

    on cascade

--- klsm_bench.sh
+++ klsm_bench.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd ./klsm
-make clean && make && ./bench.py -a klsm128,klsm256 -p 1,2,3,5,10,15,20 -r 5 -o results.csv
+make clean && make
+taskset -c 0-31 ./bench.py -a klsm128,klsm256 -p 1,8,16,24,32 -r 5 -o results.csv
 Rscript misc/plot_throughput.R results.csv
