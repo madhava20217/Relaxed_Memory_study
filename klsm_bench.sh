c2e942616fe175c87d750dab51ce29d97aa2e9ff
Chameleon Cloud User <cc@ch-cascade.novalocal>
2022-07-26 05:42:11 +0000

    script working, setting frequencies well

--- klsm_bench.sh
+++ klsm_bench.sh
@@ -8,13 +8,14 @@ sudo modprobe cpufreq_userspace
 
 ITERS=10
 
-FREQS=( 1200 1500 2000 2500 2800 )  		#List of frequencies to run the test at
+FREQS=(1200 1500 2000 2500 2800 )  		#List of frequencies to run the test at
 
 cd klsm
 
 for freq in "${FREQS[@]}"; do
+    
     sudo cpupower frequency-set --governor userspace
-    sudo cpupower frequency-set --freq "$freq"
+    sudo cpupower frequency-set --freq "$freq"000
 
     taskset -c 0-31 ./bench.py -a klsm128,klsm256 -p 1 -r $ITERS -o ./results/${freq}/results_1.csv
     taskset -c 0-31 ./bench.py -a klsm128,klsm256 -p 8 -r $ITERS -o ./results/${freq}/results_8.csv
