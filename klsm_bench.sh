74c4ea157d3e3ffaa0a7d192d4517331b1a050cc
Chameleon Cloud User <cc@ch-haswell-anuj.novalocal>
2022-07-31 13:25:43 +0000

    haswell

--- klsm_bench.sh
+++ klsm_bench.sh
@@ -10,19 +10,17 @@ sudo chmod +rw /dev/cpu/*/msr
 
 ITERS=5
 
-FREQS=( 1200000 1500000 2000000 2500000 2800000 )  	#List of frequencies to run the test at
-
+FREQS=( 1200000 1500000 1800000 2100000 2300000 )  	#List of frequencies to run the test at
+CORES=( 1 8 16 24 )
 cd klsm
 
 for freq in "${FREQS[@]}"; do
     echo "TESTING FREQUENCY $freq"
  
     # sudo cpupower frequency-set --governor userspace
-    # sudo cpupower frequency-set --freq "$freq"
-
-    ../helper.sh $freq $ITERS 1 20
-    ../helper.sh $freq $ITERS 8 20
-    ../helper.sh $freq $ITERS 16 20
-    ../helper.sh $freq $ITERS 24 20
-    ../helper.sh $freq $ITERS 32 20
+    # sudo cpupower frequency-set --freq 
+    for core in "${CORES[@]"; do
+    	echo "TESTING CORE $core"
+	../helper.sh $freq $ITERS $core 20
+    done
 done
